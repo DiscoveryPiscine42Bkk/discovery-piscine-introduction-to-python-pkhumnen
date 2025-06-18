85619c0c03b94f85d2640f4962f3d13b52214691
pkhumnen <pkhumnen@student.42bangkok.com>
2025-06-18 02:55:55 +0000

    ex02 6

---
+++ cell06/ex02/downcase_all.py
@@ -0,0 +1,9 @@
+import sys
+def downcase_it(string):
+    return string.lower()
+if name == "__main__":
+    if len(sys.argv) <= 1:
+        print("none")
+    else:
+        for arg in sys.argv[1:]:
+            print(downcase_it(arg))
